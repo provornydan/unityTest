364d61d5c0f9118fea07b49c61d77fd085a28df0
provornydan <provornydan@gmail.com>
2016-10-27 22:51:08 +0300

    last commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed;
+	private Rigidbody2D myRigidbody;
+	// Use this for initialization
+	void Start () {
+		myRigidbody = GetComponent<Rigidbody2D> (); 
+	}
+	 
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetAxisRaw ("Horizontal") > 0f)
+		{
+			myRigidbody.velocity = new Vector3(moveSpeed, 
+		}  
+	}
+}
